840fecbe7be785315c28e70d2ec83352dc45de5c
Duc Tan Hoang <ductan.hoang1407@gmail.com>
2025-11-11 12:38:13 -0800

    reformat login code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,20 +4,20 @@ import { supabase } from "../../lib/supbabaseClient";
 
 export default function LoginPage() {
   const signInWithGoogle = async () => {
-  // 1️⃣ Trigger OAuth login
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: process.env.NEXTAUTH_URL, // or your app URL
-      queryParams: { prompt: "select_account" },
-    },
-  });
+    // 1️⃣ Trigger OAuth login
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: process.env.NEXTAUTH_URL,
+        queryParams: { prompt: "select_account" },
+      },
+    });
 
-  if (error) {
-    console.error("Google login error:", error.message);
-    return;
-  }
-};
+    if (error) {
+      console.error("Google login error:", error.message);
+      return;
+    }
+  };
 
   return (
     <main className="min-h-screen flex items-center justify-center  ">
@@ -25,11 +25,12 @@ export default function LoginPage() {
         <h1 className="text-2xl font-bold mb-6">🌍 Travel App Login</h1>
 
         <div className="flex items-center justify-center">
-        <button onClick={signInWithGoogle}
-              className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          <button
+            onClick={signInWithGoogle}
+            className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
             <span className="text-black">Continue with Google</span>
-        </button>
-</div>
+          </button>
+        </div>
       </div>
     </main>
   );
